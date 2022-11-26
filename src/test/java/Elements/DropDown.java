a95ebcc5a4b7c521f3d71aa08115a9cc74055f34
Samrat <samrat.keshri.24@gmail.com>
2022-11-26 12:07:13 +0530

    Button script uploaded

---
+++ src/test/java/Elements/DropDown.java
@@ -0,0 +1,29 @@
+package Elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.sql.SQLOutput;
+import java.time.Duration;
+
+public class DropDown {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //dropdown with select tag
+        WebElement staticDropdown = driver.findElement(By.xpath("//select[@id = 'ctl00_mainContent_DropDownListCurrency']"));
+        Select dropdown = new Select(staticDropdown);
+        dropdown.selectByIndex(3);
+        System.out.println(dropdown.getFirstSelectedOption().getText());
+        dropdown.selectByVisibleText("AED");
+        System.out.println(dropdown.getFirstSelectedOption().getText());
+        System.out.println("script successfully passed");
+        driver.quit();
+    }
+}
