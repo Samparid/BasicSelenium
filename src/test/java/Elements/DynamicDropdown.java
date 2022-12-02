34599b957fd23cba26a3b75378d93dfa8b79f2c6
Samrat <samrat.keshri.24@gmail.com>
2022-12-02 12:36:21 +0530

    DynamicDropDown script uploaded

---
+++ src/test/java/Elements/DynamicDropdown.java
@@ -0,0 +1,26 @@
+package Elements;
+
+import com.sun.xml.internal.ws.server.DefaultResourceInjector;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class DynamicDropdown {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        driver.findElement(By.xpath("//a[@value='MAA']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("(//a[@value='BLR'])[2]")).click();
+        System.out.println("Script pass successfully");
+        driver.close();
+
+
+    }
+}
